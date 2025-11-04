e591217d1dc170c1d43117d9ab6701b47d13e53e
RishabhShrm02 <143181129+RishabhShrm02@users.noreply.github.com>
2025-11-04 13:25:55 +0530

    add a new file to local repo

---
+++ schoolaura/src/pages/TutorSolution.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TutorSolution = () => {
+  return (
+    <div style={{ padding: "60px", textAlign: "center" }}>
+      <h1>Tutor Solutions</h1>
+      <p>Connect with expert tutors for personalized learning support.</p>
+    </div>
+  );
+};
+
+export default TutorSolution;
